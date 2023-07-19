79d48da33d1693ebc647e26020efb6e8d4d6710a
YoussefDarwishh <youssefdarwishh@gmail.com>
2023-07-19 14:50:18 +0300

    Add project files.

---
+++ ContactDatabase/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+public class IndexModel : PageModel
+{
+    public List<Contact> Contacts { get; set; }
+
+    public void OnGet()
+    {
+        Contacts = GetContactsFromDatabase();
+    }
+
+    public IActionResult OnPost()
+    {
+        var contact = new Contact
+        {
+            FirstName = Request.Form["FirstName"],
+            LastName = Request.Form["LastName"],
+            Email = Request.Form["Email"],
+            Title = Request.Form["Title"],
+            Description = Request.Form["Description"],
+            DateOfBirth = DateTime.Parse(Request.Form["DateOfBirth"]),
+            MarriageStatus = Request.Form.ContainsKey("MarriageStatus")
+        };
+
+        return RedirectToPage();
+    }
+
+    private List<Contact> GetContactsFromDatabase()
+    {
+        return new List<Contact>();
+    }
+}
+
+public class Contact
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public bool MarriageStatus { get; set; }
+}
